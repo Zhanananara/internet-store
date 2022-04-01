085a58c1f27a608af5c5d0c43b797dfc4e540f19
Zhanananara <zhannara.nurlanova@gmail.com>
2022-04-01 16:25:52 +0600

    first

---
+++ src/MyRoutes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Admin from "./pages/Admin";
+import MainLayout from "./layuots/MainLayout";
+import Add from "./components/Admin/Add";
+import Form from "./components/Admin/Form";
+import List from "./components/Admin/List";
+import Edit from "./components/Admin/Edit";
+
+const MyRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/admin" element={<Admin />}>
+            <Route index element={<List />} />
+            <Route path="add" element={<Add />} />
+            <Route path="edit" element={<Edit />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default MyRoutes;
